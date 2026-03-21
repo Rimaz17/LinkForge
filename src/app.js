f84a1262821170378204ff4a70e66c6f4ccb94de
rimaz <rimazjr10@gmail.com>
2026-03-22 03:47:12 +0530

    Setup PostgreSQL connection and created users and urls tables

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import pool from "./config/db.js";
 
 const app = express();
 
@@ -12,6 +13,23 @@ app.use(morgan("dev"));
 // test route
 app.get("/", (req, res) => {
   res.send("API is running...");
+});
+
+app.get("/create-user-test", async (req, res) => {
+  try {
+
+    const result = await pool.query(
+      "INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING *",
+      ["test@gmail.com", "hashedpassword"]
+    );
+
+    res.json(result.rows[0]);
+
+  } catch (error) {
+
+    res.status(500).json({ error: error.message });
+
+  }
 });
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import pool from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// test DB connection
+pool.connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
