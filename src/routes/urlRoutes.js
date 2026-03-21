1733a3b71250b618b4d5c201cdebf1130eada7bf
rimaz <rimazjr10@gmail.com>
2026-03-22 05:09:06 +0530

    setup url routes

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,37 +1,13 @@
 import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import pool from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
-import urlRoutes from "./routes/urlRoutes.js";
+import { createShortUrl, redirectUrl } from "../controllers/urlController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
-const app = express();
+const router = express.Router();
 
-// middlewares
-app.use(cors());
-app.use(express.json());
-app.use(morgan("dev"));
+// create short URL (protected)
+router.post("/", authMiddleware, createShortUrl);
 
-// test route
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+// redirect (public)
+router.get("/:shortCode", redirectUrl);
 
-// DB test
-app.get("/test-db", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.json({
-      message: "DB working",
-      time: result.rows[0]
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// routes
-app.use("/api/auth", authRoutes);
-app.use("/api/urls", urlRoutes);
-
-export default app;
+export default router;
