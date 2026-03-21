85962a19edb8ba6c1d8665e0f6e74d20ff72e57a
rimaz <rimazjr10@gmail.com>
2026-03-22 04:36:30 +0530

    Implemented authentication with bcrypt and JWT

--- src/app.js
+++ src/app.js
@@ -2,8 +2,17 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import pool from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import authMiddleware from "./middleware/authMiddleware.js";
 
 const app = express();
+
+app.get("/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "Protected route",
+    user: req.user
+  });
+});
 
 // middlewares
 app.use(cors());
@@ -15,21 +24,24 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/create-user-test", async (req, res) => {
+// DB test route
+app.get("/test-db", async (req, res) => {
   try {
-
-    const result = await pool.query(
-      "INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING *",
-      ["test@gmail.com", "hashedpassword"]
-    );
-
-    res.json(result.rows[0]);
-
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      message: "DB working",
+      time: result.rows[0]
+    });
   } catch (error) {
-
     res.status(500).json({ error: error.message });
-
   }
+});
+
+// auth routes
+app.use("/api/auth", authRoutes);
+
+app.get("/protected", (req, res) => {
+  res.send("This is unprotected route");
 });
 
 export default app;
