a9c9e52854537bed89a8a3640051ff1e98d63f4c
rimaz <rimazjr10@gmail.com>
2026-03-22 06:55:14 +0530

    Added Redis caching for URL redirects

---
+++ src/config/redis.js
@@ -0,0 +1,18 @@
+import { createClient } from "redis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL
+});
+
+redisClient.on("error", (err) => {
+  console.log("Redis error:", err);
+});
+
+await redisClient.connect();
+
+console.log("Connected to Redis");
+
+export default redisClient;
